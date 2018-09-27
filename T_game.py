c7827de2c5043c5ee464dc685b4af452d8eb542e
Felix Blind <felixblind@gmail.com>
2018-09-27 13:46:02 +0200

    The classes are now a little bit more reasonable.

--- T_game.py
+++ T_game.py
@@ -4,30 +4,34 @@ import numpy as np
 from T_player import T_player
 
 class T_game():
-    
-    board_state_A = []
-    board_state_B = []
+    # This variables will stay the same over all instances of the class T_game.
+    # Every game will have a board which is 3 by 3 big.
+    # They are not immutable
     length = 3
     width = 3
+    board = [[None] * 3 for _ in range(3)]
+    
     
     def __init__(self):
-       pass 
+        # this here are instance variables. They are bound to the instance of
+        # T_game. If I instanciate another game. They will be set again here
+        # in init and will have no influence on board_states in other games.
+        self.board_state_A = []
+        self.board_state_B = []
 
-    def draw_field(player_A,player_B):
-        length = T_game.length
-        width = T_game.width
-        board = np.chararray((3, 3))
-        for row in range(length):
-                for column in range(width):
-                    print("|",end="")
-                    print("__",end="")
+    def draw_field(self, player_A, player_B):
+        board = np.chararray((T_game.length, T_game.width))
+        for row in range(T_game.length):
+                for column in range(T_game.width):
+                    print("|", end="")
+                    print("__", end="")
                     #if row == int(cross_x) and column == int(cross_y):
                     #    print("x",end="")
-                    for tuple in T_game.board_state_A:
+                    for tuple in self.board_state_A:
                         if row == int(tuple[0]) and column == int(tuple[1]):
                             print(player_A.symbol,end="")
                             board[row][column]= player_A.symbol
-                    for tuple in T_game.board_state_B:
+                    for tuple in self.board_state_B:
                         if row == int(tuple[0]) and column == int(tuple[1]):
                             print(player_B.symbol,end="")
                             board[row][column]= player_B.symbol
@@ -36,34 +40,34 @@ class T_game():
 
 
     def play(self,player_A,player_B):
-        board = T_game.draw_field(player_A,player_B) 
+        self.draw_field(player_A,player_B) 
         while True :
-            board = [[]]
             if player_A.turn == True:
-                T_game.board_state_A.append(T_player.play(player_A))
-                board = T_game.draw_field(player_A,player_B) 
+                self.board_state_A.append(T_player.play(player_A))
+                self.draw_field(player_A,player_B) 
                 player_A.turn = False
                 player_B.turn = True
             else:
-                T_game.board_state_B.append(T_player.play(player_B))
-                board = T_game.draw_field(player_A,player_B) 
+                self.board_state_B.append(T_player.play(player_B))
+                self.draw_field(player_A,player_B) 
                 player_B.turn =  False
                 player_A.turn = True
-            print(T_game.evaluate(player_A,board))
-            print(T_game.evaluate(player_B,board))
-            if (T_game.evaluate(player_A,board) and T_game.evaluate(player_B,board)):
+            print(self.evaluate(player_A))
+            print(self.evaluate(player_B))
+            if (self.evaluate(player_A) and self.evaluate(player_B)):
                 print("GAME OVER") 
                 break
 
-    def evaluate(player,board):
+    def evaluate(self, player):
             is_full = 0
             counter = 0
-            print(T_game.board_state_A)
-            print(T_game.board_state_B)
-            for row in range(3):
-                for column in range(3):
-                    #print(board[row][column])
-                    if board[row][column] == player.symbol:
+            print(self.board_state_A)
+            print(self.board_state_B)
+            for row in range(T_game.length):
+                for column in range(T_game.width):
+                    # print(f"row {row}, col: {column}")
+                    #print(T_game.board[row][column])
+                    if T_game.board[row][column] == player.symbol:
                         counter += 1
                     else:
                         continue
--- test_Tic_Tac_Toe.py
+++ test_Tic_Tac_Toe.py
@@ -3,7 +3,7 @@ from T_player import T_player
 
 battlefield = T_game()
 
-Felix = T_player(battlefield,"o",True)
-Paul = T_player(battlefield,"*",False)
+Felix = T_player(battlefield, "o", True)
+Paul = T_player(battlefield, "*", False)
 
-T_game.play(battlefield,Felix,Paul)
+battlefield.play(Felix, Paul)
